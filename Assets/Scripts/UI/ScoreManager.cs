ba05615b4fe6b76f1bb79d31c0b8e13eb564772d
DasArmin <armin.hunter@hotmail.com>
2015-09-16 19:26:57 +0200

    UI again 16-09-15

---
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour 
+{
+	[SerializeField]
+	private Text scoreText;
+
+	public float scoreCounter;
+
+	void Start () 
+	{
+		SetScoreText ();
+	}
+	
+	void Update () 
+	{
+		SetScoreText ();
+		scoreCounter++;
+	}
+
+	void SetScoreText()
+	{
+		scoreText.text = "Score: " + scoreCounter.ToString ();
+	}
+}
---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour 
+{
+    [SerializeField]
+    private Text timerText;
+
+    private float timerCounter;
+
+	void Update () 
+    {
+        timerCounter += Time.deltaTime;
+
+        var minutes = timerCounter / 60;
+        var seconds = timerCounter % 60;
+        var fraction = (timerCounter * 100) % 100;
+
+        timerText.text = string.Format(" Time: {0:00}:{1:00}:{2:00}", minutes, seconds, fraction);
+    }
+}
