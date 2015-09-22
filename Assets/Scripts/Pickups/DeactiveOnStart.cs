0c86626f66c965ddaa6ad7b184591150533325f0
18839 <18839@ma-web.nl>
2015-09-22 09:27:27 +0200

    Git Update v2 (22-9-15)

--- Assets/Scripts/Pickups/DeactiveOnStart.cs
+++ Assets/Scripts/Pickups/DeactiveOnStart.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class DeactiveOnStart : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		this.gameObject.SetActive (true);
-	}
-
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
 }
